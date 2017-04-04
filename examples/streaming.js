ad9c69e4fef222909b6379877c93d4dd879ebcbb
vanita5 <mail@vanit.as>
2017-04-04 12:17:55 +0200

    add streaming example

---
+++ examples/streaming.js
@@ -0,0 +1,15 @@
+import Mastodon from '../lib/mastodon'
+
+const M = new Mastodon({
+    access_token: '...'
+})
+
+const stream = M.stream('streaming/user')
+
+stream.on('message', (msg) => {
+    console.log(msg)
+})
+
+stream.on('error', (err) => {
+    console.log(err)
+})
